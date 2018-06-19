f54b458822e4841413f35b81e3119290981604cf
Russ Garrett <russ@garrett.co.uk>
2018-06-19 08:51:24 +0100

    Version -> 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='slotmachine',
-      version='0.0.1',
+      version='0.0.2',
       description='Conference talk scheduler',
       author='EMF',
       author_email='russ@emfcamp.org',
